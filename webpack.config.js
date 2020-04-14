fdf34a8dc14cdfc10b0bf905b92a9cccb5a202dc
Irene Prins <ireneprins11@hotmail.com>
2020-04-14 14:57:00 +0200

    add basic styling eye

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|mp3)$/i,
         loader: 'file-loader',
         options: {
           name: 'assets/[name].[ext]',
