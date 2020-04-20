f1493d321e0591143bced622ed73d0b8336ca04a
Irene Prins <ireneprins11@hotmail.com>
2020-04-20 20:47:39 +0200

    add audio and start audio

--- src/js/eyes.js
+++ src/js/eyes.js
@@ -6,9 +6,16 @@ let musicWrapper = document.body.querySelector('.music-button');
 
 let stay = document.body.querySelector('#audio__stay');
 let stayaway = document.body.querySelector('#audio__stayaway')
-let comeback = document.body.querySelector('#audio__comeback')
 let begin = document.body.querySelector('#audio__begin')
 let leave = document.body.querySelector('#audio__leave')
+
+let backbutton = document.body.querySelector('.back-button')
+
+let audio1 = document.body.querySelector('#audio-bg1')
+let audio2 = document.body.querySelector('#audio-bg2')
+let audio3 = document.body.querySelector('#audio-bg3')
+
+let p = document.body.querySelector('.music-button__text')
 
 function init() {
     getButton()
@@ -20,36 +27,31 @@ wrapper.addEventListener('mouseleave', () => {
 
 musicWrapper.addEventListener('mouseover', () => {
     stayaway.play()
+    stayaway.volume = 1
 })
 
-// window.addEventListener('mouseover', () => {
-//     // begin.play()
-// })
+backbutton.addEventListener('mouseover', () => {
+    stay.play()
+})
 
 function getButton() {
     let audioButton = document.body.querySelector('.music-button');
-    let audio1 = document.body.querySelector('#audio-bg1')
-    let audio2 = document.body.querySelector('#audio-bg2')
-    let audio3 = document.body.querySelector('#audio-bg3')
-    let p = document.body.querySelector('.music-button__text')
 
     audioButton.addEventListener("click", (e) => {
         let target = e.target
         if(target.classList.contains('music-button__image1')) {
             p.innerHTML = 'Now playing: I follow you'
-            audio1.volume = 0.3
-            console.log(audio1.volume)
+            audio1.volume = 0.1;
             return audio1.paused ? audio1.play() : audio1.pause();
         } else if(target.classList.contains('music-button__image2')) {
             p.innerHTML = 'Now playing: Crazy Eyes'
-            audio2.volume = 0.3
+            audio2.volume = 0.1
             return audio2.paused ? audio2.play() : audio2.pause();
         } else {
             p.innerHTML = 'Now playing: Eyes on you'
-            audio3.volume = 0.3
+            audio3.volume = 0.1
             return audio3.paused ? audio3.play() : audio3.pause();
         }
-        
     })
 }
 
@@ -69,7 +71,15 @@ let modal = document.body.querySelector('.modal')
 let closeButton = document.body.querySelector('.concept__button')
 
 closeButton.addEventListener('click', () => {
+    begin.play()
     modal.style.display = "none"
 })
+
+window.onload = function() {
+    audio1.play();
+    audio1.volume = 0.2;
+
+    p.innerHTML = 'Now playing: I follow you'
+}
 
 window.addEventListener("load", init)
